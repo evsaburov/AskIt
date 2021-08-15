1b17fc43d6348dcb02e1370fc4166ca8a6f3a72b
Saburov Evgeniy <evs.zlat@gmail.com>
2021-08-15 10:24:43 +0500

    form update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,18 @@
 class QuestionsController < ApplicationController
+    
+    def edit
+        @question = Question.find_by id: params[:id]
+    end
+
+    def update
+        @question = Question.find_by id: params[:id]
+        if @question.update question_params
+            redirect_to questions_path
+        else
+            render :new
+        end
+    end
+    
     def index
         @questions = Question.all
     end
@@ -16,6 +30,8 @@ class QuestionsController < ApplicationController
             render :new
         end
     end
+
+    private
 
     def question_params
         params.require(:question).permit(:title, :body)

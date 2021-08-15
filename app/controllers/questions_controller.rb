16c9ff4e79e01e957216c73b94c5646e8f548612
Saburov Evgeniy <evs.zlat@gmail.com>
2021-08-15 07:50:48 +0500

    new form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,23 @@
-class QuestionsController < ActionController::Base
+class QuestionsController < ApplicationController
     def index
         @questions = Question.all
+    end
+
+    def  new
+        @question = Question.new
+    end
+
+    def create
+        # render plain: params
+        @question = Question.new question_params
+        if @question.save
+            redirect_to questions_path
+        else
+            render :new
+        end
+    end
+
+    def question_params
+        params.require(:question).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/questions', to: 'questions#index'
+  post '/questions', to: 'questions#create'
+  get '/questions/new', to: 'questions#new'
 end
